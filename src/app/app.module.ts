765017f7fe9d177b220d2c6ab91b8f0c01f03021
MukeshKumar101 <maxw2764@gmail.com>
2021-01-20 13:06:52 +0530

    added essentials

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRouting } from './app-routing.module';
-import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [BrowserModule, AppRouting, HttpClientModule],
   bootstrap: [AppComponent],
 })
